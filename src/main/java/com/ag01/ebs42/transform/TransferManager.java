0eadc89b10732339d3fec8fbb0a305c0012fbf86
name <mail>
2019-07-18 13:25:00 +0200

    Methoden für notwendige Tabellen

--- src/main/java/com/ag01/ebs42/transform/TransferManager.java
+++ src/main/java/com/ag01/ebs42/transform/TransferManager.java
@@ -13,7 +13,13 @@ import org.springframework.transaction.TransactionException;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
 
+import com.ag01.ebs42.analyze_eap_db.database_access.data_access_object.api.TconnectorDao;
+import com.ag01.ebs42.analyze_eap_db.database_access.data_access_object.api.TobjectDao;
+import com.ag01.ebs42.analyze_eap_db.database_access.data_access_object.api.TobjectpropertiesDao;
 import com.ag01.ebs42.analyze_eap_db.database_access.data_access_object.api.TpackageDao;
+import com.ag01.ebs42.analyze_eap_db.database_access.domain_object.TconnectorDo;
+import com.ag01.ebs42.analyze_eap_db.database_access.domain_object.TobjectDo;
+import com.ag01.ebs42.analyze_eap_db.database_access.domain_object.TobjectpropertiesDo;
 import com.ag01.ebs42.analyze_eap_db.database_access.domain_object.TpackageDo;
 
 public class TransferManager 
@@ -25,7 +31,13 @@ public class TransferManager
 	private static Logger LOGGER = LogManager.getLogger(TransferManager.class);
 	
 	private TpackageDao tpackageDao;
+	private TobjectDao tobjectDao;
+	private TobjectpropertiesDao tobjectpropertiesDao;
+	private TconnectorDao tconnectorDao;
 	private List <TpackageDo> resultTpackageDoList;
+	private List <TobjectDo> resultTobjectDoList;
+	private List <TobjectpropertiesDo> resultTobjectpropertiesDoList;
+	private List <TconnectorDo> resultTconnectorDoList1;
 	
 	public TransferManager() { }
 	
@@ -77,7 +89,7 @@ public class TransferManager
 		}
 	}
 	
-	public void setTpackageDao() 
+	public List<TpackageDo> setTpackageDao() 
 	{		
 		TpackageDao tpackageDao = (TpackageDao) this.appContext.getBean("tpackageDao");
         List<TpackageDo> resultTpackageDoList = null;
@@ -106,5 +118,109 @@ public class TransferManager
             }
         }
         this.tpackageDao = tpackageDao;
+        return resultTpackageDoList;
+	}
+	
+	public List<TobjectDo> setTobjectDao()
+	{
+        TobjectDao tobjectDao = (TobjectDao) this.appContext.getBean("tobjectDao");
+        List<TobjectDo> resultTobjectList = null;
+        
+        try 
+		{
+			resultTobjectList = tobjectDao.readListTobject();
+		}
+		catch (Exception e) 
+		{
+			LOGGER.error("Exception while reading t_object\n" + e.getMessage());
+		}
+		
+        if(resultTobjectList != null) 
+        {
+            for(int i = 0; i < resultTobjectList.size(); i++) 
+            {
+                TobjectDo tobjectDo = resultTobjectList.get(i);
+                if(tobjectDo != null) 
+                {
+                    LOGGER.info("object[" + i + "] = " + tobjectDo.toString());
+                } 
+                else 
+                {
+                    LOGGER.info("object[" + i + "] = null");
+                }
+            }
+        }
+        this.tobjectDao = tobjectDao;
+        
+        return resultTobjectList;
+	}
+	
+	
+	public List<TobjectpropertiesDo> setTobjectpropertiesDao()
+	{
+		TobjectpropertiesDao tobjectpropertiesDao = (TobjectpropertiesDao)this.appContext.getBean("tobjectpropertiesDao");
+		List<TobjectpropertiesDo> resultTobjectpropertiesDoList = null;
+		
+		try 
+		{
+			resultTobjectpropertiesDoList = tobjectpropertiesDao.readListTobjectproperties();
+		}
+		catch (Exception e) 
+		{
+			LOGGER.error("Exception while reading t_object\n" + e.getMessage());
+		}
+		
+        if(resultTobjectpropertiesDoList != null) 
+        {
+            for(int i = 0; i < resultTobjectpropertiesDoList.size(); i++) 
+            {
+            	TobjectpropertiesDo tobjectpropertiesDo = resultTobjectpropertiesDoList.get(i);
+                if(tobjectpropertiesDo != null) 
+                {
+                    LOGGER.info("object[" + i + "] = " + tobjectpropertiesDo.toString());
+                } 
+                else 
+                {
+                    LOGGER.info("object[" + i + "] = null");
+                }
+            }
+        }
+        this.tobjectpropertiesDao = tobjectpropertiesDao;
+		
+		return resultTobjectpropertiesDoList;
+	} 
+	
+	public List <TconnectorDo> setTconnectorDao()
+	{
+        TconnectorDao tconnectorDao = (TconnectorDao)this.appContext.getBean("tconnectorDao");
+        List <TconnectorDo> resultTconnectorDoList1 = null;
+        
+        try 
+		{
+        	resultTconnectorDoList1 = tconnectorDao.readListTconnector();
+		}
+		catch (Exception e) 
+		{
+			LOGGER.error("Exception while reading t_object\n" + e.getMessage());
+		}
+		
+        if(resultTconnectorDoList1 != null) 
+        {
+            for(int i = 0; i < resultTconnectorDoList1.size(); i++) 
+            {
+            	TconnectorDo tconnectorDo = resultTconnectorDoList1.get(i);
+                if(tconnectorDo != null) 
+                {
+                    LOGGER.info("object[" + i + "] = " + tconnectorDo.toString());
+                } 
+                else 
+                {
+                    LOGGER.info("object[" + i + "] = null");
+                }
+            }
+        }
+        this.tconnectorDao = tconnectorDao;
+        
+        return resultTconnectorDoList1;
 	}
 }
