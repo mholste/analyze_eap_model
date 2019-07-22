8e1da66ea72ba4b86cc49ec8e326610bdcc16819
name <mail>
2019-07-22 14:57:37 +0200

    interface now

--- src/main/java/com/ag01/ebs42/transform/wrapper/Arc42SystemComponentWrapper.java
+++ src/main/java/com/ag01/ebs42/transform/wrapper/Arc42SystemComponentWrapper.java
@@ -1,9 +1,51 @@
 package com.ag01.ebs42.transform.wrapper;
 
-public interface Arc42SystemComponentWrapper 
+import com.ag01.ebs42.meta_model.api.Arc42SystemComponent;
+
+public abstract class Arc42SystemComponentWrapper implements Arc42SystemComponent
 {
-	public String geteaId();
-	public void seteaId(String eaId);
-	public String geteaPackageId();
-	public void seteaPackageId(String eaPackageId);
+	protected Arc42SystemComponent wrappedSystemComponent;
+	
+	public Arc42SystemComponentWrapper(Arc42SystemComponent wrappedSystemComponent)
+	{
+		this.wrappedSystemComponent = wrappedSystemComponent;
+	}
+
+	@Override
+	public String getPackageName() 
+	{
+		return wrappedSystemComponent.getPackageName();
+	}
+
+	@Override
+	public void setPackageName(String packageName) 
+	{
+		wrappedSystemComponent.setPackageName(packageName);
+	}
+
+	@Override
+	public String getSystemName() 
+	{
+		return wrappedSystemComponent.getSystemName();
+	}
+
+	@Override
+	public void setSystemName(String systemName) 
+	{
+		wrappedSystemComponent.setSystemName(systemName);
+	}
+
+	@Override
+	public String getCorporateID() 
+	{
+		return wrappedSystemComponent.getCorporateID();
+	}
+
+	@Override
+	public void setCorporateIDe(String corporateID) 
+	{
+		wrappedSystemComponent.setCorporateIDe(corporateID);
+	}
+	
+	
 }
