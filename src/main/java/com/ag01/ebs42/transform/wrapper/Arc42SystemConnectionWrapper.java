46d248e3e76a758e597283a03c91b9133ff8f156
name <mail>
2019-07-23 16:20:55 +0200

    initial

---
+++ src/main/java/com/ag01/ebs42/transform/wrapper/Arc42SystemConnectionWrapper.java
@@ -0,0 +1,15 @@
+package com.ag01.ebs42.transform.wrapper;
+
+import com.ag01.ebs42.meta_model.api.Arc42SystemConnection;
+
+public abstract class Arc42SystemConnectionWrapper implements Arc42SystemConnection 
+{
+protected Arc42SystemConnection wrappedSystemConnection;
+	
+	public Arc42SystemConnectionWrapper(Arc42SystemConnection wrappedSystemConnection)
+	{
+		this.wrappedSystemConnection = wrappedSystemConnection;
+	}
+	
+	
+}
