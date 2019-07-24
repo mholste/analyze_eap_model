2c3cec9dfa857b26f53b48a6d9b97bcc623f50be
name <mail>
2019-07-24 10:48:23 +0200

    initial

---
+++ src/main/java/com/ag01/ebs42/model/arc42/api/Arc42SystemComponent.java
@@ -0,0 +1,22 @@
+package com.ag01.ebs42.model.arc42.api;
+
+import java.util.List;
+
+import com.ag01.ebs42.model.arc42.domobj.Arc42SystemDo;
+import com.ag01.ebs42.model.arc42.domobj.Arc42SystemInterface;
+
+public interface Arc42SystemComponent 
+{
+	public long getuId();
+	public void setuId(long uId);
+	public String getSystemComponentName();
+	public void setSystemComponentName(String systemComponentName);
+	public String getPackageName();
+	public void setPackageName(String packageName);
+	public Arc42SystemDo getBaseSystem();
+	public void setBaseSystem(Arc42SystemDo baseSystem);
+	public List<Arc42SystemInterface> getServerInterfaces() ;
+	public void setServerInterfaces(List<Arc42SystemInterface> serverInterfaces);
+	public List<Arc42SystemInterface> getClientInterfaces();
+	public void setClientInterfaces(List<Arc42SystemInterface> clientInterfaces);
+}
