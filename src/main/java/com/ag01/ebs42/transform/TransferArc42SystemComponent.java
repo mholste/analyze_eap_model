08841e7f411023972816f52281db8208002a0fad
name <mail>
2019-07-24 13:02:21 +0200

    after implementation of correct interface

--- src/main/java/com/ag01/ebs42/transform/TransferArc42SystemComponent.java
+++ src/main/java/com/ag01/ebs42/transform/TransferArc42SystemComponent.java
@@ -1,56 +1,35 @@
 package com.ag01.ebs42.transform;
 
-import com.ag01.ebs42.meta_model.api.Arc42SystemComponent;
+import java.util.List;
+
+import com.ag01.ebs42.model.arc42.api.Arc42SystemComponent;
+import com.ag01.ebs42.model.arc42.domobj.Arc42SystemDo;
+import com.ag01.ebs42.model.arc42.domobj.Arc42SystemInterface;
 import com.ag01.ebs42.transform.wrapper.Arc42SystemComponentWrapper;
 
 public class TransferArc42SystemComponent extends Arc42SystemComponentWrapper
 {
 	private String eaId;
 	private String eaPackageId;
+	private String corporateId;
 	
 	public TransferArc42SystemComponent(Arc42SystemComponent wrappedSystemComponent) {
 		super(wrappedSystemComponent);
 	}
 	
-	@Override
-	public String getPackageName()
+	public String getCorporateId()
 	{
-		return wrappedSystemComponent.getPackageName();
+		return this.corporateId;
 	}
 	
-	@Override
-	public void setPackageName(String packageName) 
+	public void setCorporateId(String id)
 	{
-		wrappedSystemComponent.setPackageName(packageName);
+		this.corporateId = id;
 	}
-
-	@Override
-	public String getSystemName() 
-	{
-		return wrappedSystemComponent.getSystemName();
-	}
-
-	@Override
-	public void setSystemName(String systemName) 
-	{
-		wrappedSystemComponent.setSystemName(systemName);
-	}
-
-	@Override
-	public String getCorporateID() 
-	{
-		return wrappedSystemComponent.getCorporateID();
-	}
-
-	@Override
-	public void setCorporateID(String corporateID) 
-	{
-		wrappedSystemComponent.setCorporateID(corporateID);
-	}
-
+	
 	public String getEaId() 
 	{
-		return eaId;
+		return this.eaId;
 	}
 
 	public void setEaId(String eaId) 
@@ -60,11 +39,83 @@ public class TransferArc42SystemComponent extends Arc42SystemComponentWrapper
 
 	public String getEaPackageId() 
 	{
-		return eaPackageId;
+		return this.eaPackageId;
 	}
 
 	public void setEaPackageId(String eaPackageId) 
 	{
 		this.eaPackageId = eaPackageId;
 	}	
+	
+	@Override
+	public String getPackageName()
+	{
+		return super.getPackageName();
+	}
+	
+	@Override
+	public void setPackageName(String packageName) 
+	{
+		super.setPackageName(packageName);
+	}	
+
+	@Override
+	public long getuId() 
+	{
+		return super.getuId();
+	}
+
+	@Override
+	public void setuId(long uId) 
+	{
+		super.setuId(uId);
+	}
+
+	@Override
+	public String getSystemComponentName() 
+	{
+		return super.getSystemComponentName();
+	}
+
+	@Override
+	public void setSystemComponentName(String systemComponentName) 
+	{
+		super.setSystemComponentName(systemComponentName);
+	}
+
+	@Override
+	public Arc42SystemDo getBaseSystem() 
+	{
+		return super.getBaseSystem();
+	}
+
+	@Override
+	public void setBaseSystem(Arc42SystemDo baseSystem)
+	{
+		super.setBaseSystem(baseSystem);
+	}
+
+	@Override
+	public List<Arc42SystemInterface> getServerInterfaces() 
+	{
+		return super.getServerInterfaces();
+	}
+
+	@Override
+	public void setServerInterfaces(List<Arc42SystemInterface> serverInterfaces) 
+	{
+		super.setServerInterfaces(serverInterfaces);
+	}
+
+	@Override
+	public List<Arc42SystemInterface> getClientInterfaces() 
+	{
+		return super.getClientInterfaces();
+	}
+
+	@Override
+	public void setClientInterfaces(List<Arc42SystemInterface> clientInterfaces) 
+	{
+		super.setClientInterfaces(clientInterfaces);
+	}
 }
