23edf8f36d2f761ea2b5fb900e7bb327bdcb77a3
mholste <michael.holste@web.de>
2019-07-28 09:11:30 +0200

    some refactoring

--- src/main/java/com/ag01/ebs42/export/ExcelExporter.java
+++ src/main/java/com/ag01/ebs42/export/ExcelExporter.java
@@ -3,10 +3,18 @@ package com.ag01.ebs42.export;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.DefaultIndexedColorMap;
+import org.apache.poi.xssf.usermodel.XSSFCellStyle;
+import org.apache.poi.xssf.usermodel.XSSFColor;
+import org.apache.poi.xssf.usermodel.XSSFFont;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
@@ -21,42 +29,82 @@ import com.ag01.ebs42.transform.TransferArc42SystemComponent;
  */
 public class ExcelExporter 
 {
-	public ExcelExporter() 
+	private static ExcelExporter instance;
+	
+	private XSSFWorkbook workbook; 
+	private XSSFFont font;
+	private XSSFCellStyle style;
+	
+	private static Logger LOGGER = LogManager.getLogger(ExcelExporter.class);
+	
+	private ExcelExporter() 
 	{
-		
+		this.workbook = new XSSFWorkbook();
+		this.font = this.workbook.createFont();
+		this.style = workbook.createCellStyle();
+	}
+	
+	public static ExcelExporter getInstance()
+	{
+		if (ExcelExporter.instance == null)
+		{
+			ExcelExporter.instance = new ExcelExporter();
+		}
+		return ExcelExporter.instance;
 	}
 	
 	public void exportSystems(List<TransferArc42SystemComponent> componentList)
 	{
-		XSSFWorkbook workbook = new XSSFWorkbook(); 
-		XSSFSheet sheet = workbook.createSheet("Systeme");
-		
-		// Create headers
-		Row headRow = sheet.createRow(1);
-		Cell cell1 = headRow.createCell(1);
-		cell1.setCellValue("System");
-		Cell cell2 = headRow.createCell(2);
-		cell2.setCellValue("ID");
+		XSSFSheet sheet = workbook.createSheet("Systeme");		
+		ArrayList<String> headers = new ArrayList<String>(Arrays.asList("ID", "System"));
+		this.createHeaders(headers, sheet);
+		this.font.setBold(false);
+		this.style.setFont(font);
+		byte[] rgb = { (byte) 255, (byte) 255, (byte) 255};		
+		this.style.setFillBackgroundColor(new XSSFColor(rgb, new DefaultIndexedColorMap()));
 		
 		int rownum = 1;
 		for (TransferArc42SystemComponent comp : componentList)
 		{
 			Row row = sheet.createRow(rownum++);
-			Cell sysCell = row.createCell(1);
+			Cell sysCell = row.createCell(0);
 			sysCell.setCellValue(comp.getSystemComponentName());
-			Cell idCell = row.createCell(2);
+			Cell idCell = row.createCell(1);
 			idCell.setCellValue(comp.getCorporateId());
 		}
 		
-		try
+		try 
 		{
-			FileOutputStream out = new FileOutputStream(new File("report.xlsx"));
-			workbook.write(out);
-			out.close();
+			this.generateReport();
+		} 
+		catch (IOException e) 
+		{
+			LOGGER.error("Exception  generating report\n" + e.getMessage(), e);
 		}
-		catch(IOException ioe)
+	}
+	
+	private void generateReport() throws IOException
+	{
+		FileOutputStream out = new FileOutputStream(new File("report.xlsx"));
+		workbook.write(out);
+		out.close();
+	}
+	
+	private void createHeaders (ArrayList<String> headers, XSSFSheet sheet)
+	{
+		this.font.setBold(true);
+		byte[] rgb = { 120, 100, (byte) 200};		
+		this.style.setFillBackgroundColor(new XSSFColor(rgb, new DefaultIndexedColorMap()));
+		this.style.setFont(font);
+		
+		int cellNum = 0;
+		Row headRow = sheet.createRow(0);
+		
+		for (String head :headers)
 		{
-			ioe.printStackTrace();
+			Cell cell = headRow.createCell(cellNum++);
+			cell.setCellValue(head);
+			cell.setCellStyle(style);
 		}
 	}
 
