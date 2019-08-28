3bba9102b48cf6290a7072eb339fc31c221687a2
name <mail>
2019-08-28 16:02:58 +0200

    description

--- src/main/java/com/ag01/ebs42/model/arc42/ConnectionType.java
+++ src/main/java/com/ag01/ebs42/model/arc42/ConnectionType.java
@@ -2,12 +2,19 @@ package com.ag01.ebs42.model.arc42;
 
 public enum ConnectionType 
 {
-	DEPENDENCY,
-	GENERALIZATIOM,
-	AGGREGATION,
-	ASSOCIATION,
-	REALISATION,
-	CONTROL_FLOW,
-	OBJECT_FLOW,
-	INFORMATION_FLOW
+	DEPENDENCY("Dependency"),
+	GENERALIZATIOM("Generalization"),
+	AGGREGATION("Aggregation"),
+	ASSOCIATION("Association"),
+	REALISATION("Realization"),
+	CONTROL_FLOW("ControlFlow"),
+	OBJECT_FLOW("ObjectFlow"),
+	INFORMATION_FLOW("InformationFlow");
+	
+	private final String description;
+	
+	ConnectionType(String desc)
+	{
+		description = desc;
+	}
 }
