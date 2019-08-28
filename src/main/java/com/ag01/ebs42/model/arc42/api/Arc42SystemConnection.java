c752c6e35365d0c438a5f0cef5a8459cd7a0fa5c
name <mail>
2019-08-28 15:19:09 +0200

    enums für connector und direction

--- src/main/java/com/ag01/ebs42/model/arc42/api/Arc42SystemConnection.java
+++ src/main/java/com/ag01/ebs42/model/arc42/api/Arc42SystemConnection.java
@@ -1,5 +1,8 @@
 package com.ag01.ebs42.model.arc42.api;
 
+import com.ag01.ebs42.model.arc42.ConnectionType;
+import com.ag01.ebs42.model.arc42.DirectionType;
+
 public interface Arc42SystemConnection 
 {
 	public String getInterface();
@@ -7,5 +10,11 @@ public interface Arc42SystemConnection
 	public String getProvidedSystem();
 	public void setProvidedSystem(String providedSystem);
 	public String getRequiredSystem();
-	public void setRequiredSystem(String requiredSystem);	
+	public void setRequiredSystem(String requiredSystem);
+	public String getConnectionName();
+	public void setConnectionName(String connectionName);
+	public ConnectionType getConnectorType();
+	public void setConnectorType(ConnectionType type);
+	public DirectionType getDirection();
+	public void setDirection(DirectionType direction);
 }
