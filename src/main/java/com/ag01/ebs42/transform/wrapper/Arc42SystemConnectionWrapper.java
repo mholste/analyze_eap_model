e0414e898dcd8d5abf9098ff041e507a73d43e5c
name <mail>
2019-08-28 15:28:13 +0200

    after update interface Arc42SystemConnection

--- src/main/java/com/ag01/ebs42/transform/wrapper/Arc42SystemConnectionWrapper.java
+++ src/main/java/com/ag01/ebs42/transform/wrapper/Arc42SystemConnectionWrapper.java
@@ -1,5 +1,7 @@
 package com.ag01.ebs42.transform.wrapper;
 
+import com.ag01.ebs42.model.arc42.ConnectionType;
+import com.ag01.ebs42.model.arc42.DirectionType;
 import com.ag01.ebs42.model.arc42.api.Arc42SystemConnection;
 
 public abstract class Arc42SystemConnectionWrapper implements Arc42SystemConnection 
@@ -9,6 +11,78 @@ protected Arc42SystemConnection wrappedSystemConnection;
 	public Arc42SystemConnectionWrapper(Arc42SystemConnection wrappedSystemConnection)
 	{
 		this.wrappedSystemConnection = wrappedSystemConnection;
+	}
+
+	@Override
+	public String getInterface() 
+	{
+		return wrappedSystemConnection.getInterface();
+	}
+
+	@Override
+	public void setInterface(String interfaceName) 
+	{
+		wrappedSystemConnection.setInterface(interfaceName);
+	}
+
+	@Override
+	public String getProvidedSystem() 
+	{
+		return wrappedSystemConnection.getProvidedSystem();
+	}
+
+	@Override
+	public void setProvidedSystem(String providedSystem) 
+	{
+		wrappedSystemConnection.setProvidedSystem(providedSystem);
+	}
+
+	@Override
+	public String getRequiredSystem() 
+	{
+		return wrappedSystemConnection.getRequiredSystem();
+	}
+
+	@Override
+	public void setRequiredSystem(String requiredSystem) 
+	{
+		wrappedSystemConnection.setRequiredSystem(requiredSystem);
+	}
+
+	@Override
+	public String getConnectionName() 
+	{
+		return wrappedSystemConnection.getConnectionName();
+	}
+
+	@Override
+	public void setConnectionName(String connectionName) 
+	{
+		wrappedSystemConnection.setConnectionName(connectionName);
+	}
+
+	@Override
+	public ConnectionType getConnectorType() 
+	{
+		return wrappedSystemConnection.getConnectorType();
+	}
+
+	@Override
+	public void setConnectorType(ConnectionType type) 
+	{
+		wrappedSystemConnection.setConnectorType(type);
+	}
+
+	@Override
+	public DirectionType getDirection() 
+	{
+		return wrappedSystemConnection.getDirection();
+	}
+
+	@Override
+	public void setDirection(DirectionType direction) 
+	{
+		wrappedSystemConnection.setDirection(direction);
 	}
 	
 	
