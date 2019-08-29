f9edcf97fd8d9f3a2adb5f370a2bf8882e5200c1
name <mail>
2019-08-29 13:56:08 +0200

    inserted description

--- src/main/java/com/ag01/ebs42/model/utils/DirectionType.java
+++ src/main/java/com/ag01/ebs42/model/utils/DirectionType.java
@@ -2,7 +2,15 @@ package com.ag01.ebs42.model.utils;
 
 public enum DirectionType 
 {
-	SOURCE_DEST,
-	DEST_SOURCE,
-	BIDIRECTIONAL
+	SOURCE_DEST("Source -> Destination"),
+	DEST_SOURCE("Destination -> Source"),
+	UNSPECIFIED("Unspecified"),
+	BIDIRECTIONAL("Bi-Directional");
+	
+	private final String description;
+	
+	DirectionType(String desc)
+	{
+		description = desc;
+	}
 }
