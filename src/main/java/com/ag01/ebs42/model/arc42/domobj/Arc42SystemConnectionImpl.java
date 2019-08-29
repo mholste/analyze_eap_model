fae7042083926137cb7d70acd35a4694b4211fac
name <mail>
2019-08-29 09:12:54 +0200

    initial

---
+++ src/main/java/com/ag01/ebs42/model/arc42/domobj/Arc42SystemConnectionImpl.java
@@ -0,0 +1,89 @@
+package com.ag01.ebs42.model.arc42.domobj;
+
+import java.io.Serializable;
+
+import com.ag01.ebs42.model.arc42.ConnectionType;
+import com.ag01.ebs42.model.arc42.DirectionType;
+import com.ag01.ebs42.model.arc42.api.Arc42SystemConnection;
+
+public class Arc42SystemConnectionImpl implements Serializable, Arc42SystemConnection 
+{
+	private static final long serialVersionUID = 1305793179181261351L;
+	
+	private long uId;
+	private String interfaceName;
+	private String 
+
+	@Override
+	public String getInterface() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setInterface(String interfaceName) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getProvidedSystem() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setProvidedSystem(String providedSystem) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getRequiredSystem() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setRequiredSystem(String requiredSystem) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getConnectionName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setConnectionName(String connectionName) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ConnectionType getConnectorType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setConnectorType(ConnectionType type) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public DirectionType getDirection() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setDirection(DirectionType direction) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
